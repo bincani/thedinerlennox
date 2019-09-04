d45968ef04ea8ed6feaafb087773f880185811ce
Ben Incani <ben.incani@gmail.com>
2019-09-04 15:32:55 +1000

    add tagline to header + highlight current day

--- wp-content/themes/airi-child/functions.php
+++ wp-content/themes/airi-child/functions.php
@@ -21,3 +21,28 @@ function my_theme_enqueue_styles() {
         true
     );
 }
+
+/**
+ * Site branding
+ */
+if ( !function_exists( 'airi_site_branding' ) ) {
+	function airi_site_branding() {
+	    echo "<div class='site-logo'>";
+		if ( has_custom_logo() ) :
+			the_custom_logo();
+		else :
+			if ( is_front_page() && is_home() ) : ?>
+				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+			<?php else : ?>
+				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+			<?php
+			endif;
+		endif;
+		$description = get_bloginfo( 'description', 'display' );
+		if ( $description || is_customize_preview() ) : ?>
+			<div class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></div>
+		<?php
+		endif;
+		echo "</div>";
+	}
+}
--- wp-content/themes/airi-child/js/script.js
+++ wp-content/themes/airi-child/js/script.js
@@ -11,4 +11,16 @@ jQuery(document).ready(function() {
     jQuery("#menu-item-487 > a").click(function(e) {
         e.preventDefault();
     });
+
+
+    var today = new Date().toLocaleDateString('en', {weekday:'short'});
+    var listItems = jQuery("#opening li");
+    listItems.each(function(idx, li) {
+        //console.log(jQuery(li).text() + ':' + jQuery(li).text().indexOf(today));
+        if (jQuery(li).text().indexOf(today) == 0) {
+            jQuery(li).addClass("open");
+        }
+    });
+
 });
+
